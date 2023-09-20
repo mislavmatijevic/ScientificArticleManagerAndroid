4c3f17db1fbf3f5d25b657a062c07a3602c3ae26
Mislav Matijević <mmatijevi@foi.hr>
2023-09-20 13:24:51 +0200

    Define AllArticlesViewModel. Load mock articles in constructor.

--- app/src/main/java/hr/foi/scientificarticlemanager/AllArticlesViewModel.kt
+++ app/src/main/java/hr/foi/scientificarticlemanager/AllArticlesViewModel.kt
@@ -1,7 +1,15 @@
 package hr.foi.scientificarticlemanager
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import hr.foi.scientificarticlemanager.helpers.MockScientificArticleRepository
+import hr.foi.scientificarticlemanager.models.ScientificArticle
 
 class AllArticlesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var articles: MutableLiveData<List<ScientificArticle>>
+
+    init {
+        this.articles = MutableLiveData<List<ScientificArticle>>()
+        this.articles.value = MockScientificArticleRepository.getArticles()
+    }
 }
